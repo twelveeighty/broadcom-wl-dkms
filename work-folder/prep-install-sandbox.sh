f030acc5e0eb3818b0fd5eabcab5a9357f29a952
Frank Vanderham <fvanderham@ontracksconsulting.com>
2014-07-19 12:52:55 -0600

    Version 6.30.223.248-1 changes

--- work-folder/prep-install-sandbox.sh
+++ work-folder/prep-install-sandbox.sh
@@ -9,7 +9,8 @@ mkdir ../install-sandbox/broadcom-wl-dkms
 # copy files
 cp -v ./broadcom-wl-dkms.install ../install-sandbox/broadcom-wl-dkms
 cp -v ./PKGBUILD ../install-sandbox/broadcom-wl-dkms
-cp -v ./hybrid-v35_64-nodebug-pcoem-6_30_223_141.tar.gz ../install-sandbox/broadcom-wl-dkms
+cp -v ./.AURINFO ../install-sandbox/broadcom-wl-dkms
+cp -v ./hybrid-v35_64-nodebug-pcoem-6_30_223_248.tar.gz ../install-sandbox/broadcom-wl-dkms
 cp -v ./broadcom-wl-dkms.conf ../install-sandbox/broadcom-wl-dkms
 cp -v ./dkms.conf ../install-sandbox/broadcom-wl-dkms
 cp -v ./license.patch ../install-sandbox/broadcom-wl-dkms
--- work-folder/prep-submit-sandbox.sh
+++ work-folder/prep-submit-sandbox.sh
@@ -6,7 +6,8 @@ rm -Rfv ../submit-sandbox/*
 # copy files
 cp -v ./broadcom-wl-dkms.install ../submit-sandbox
 cp -v ./PKGBUILD ../submit-sandbox
-cp -v ./hybrid-v35_64-nodebug-pcoem-6_30_223_141.tar.gz ../submit-sandbox
+cp -v ./.AURINFO ../submit-sandbox
+cp -v ./hybrid-v35_64-nodebug-pcoem-6_30_223_248.tar.gz ../submit-sandbox
 cp -v ./broadcom-wl-dkms.conf ../submit-sandbox
 cp -v ./dkms.conf ../submit-sandbox
 cp -v ./license.patch ../submit-sandbox
