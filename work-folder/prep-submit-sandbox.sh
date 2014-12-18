375b96ee110f142edd70527f811251785c293ee1
Frank Vanderham <fvanderham@ontracksconsulting.com>
2014-12-17 20:35:45 -0700

    Adopted linux-recent.patch from AUR broadcom-wl package. Adopted SRCINFO approach instead of AURINFO approach. Bumped package version to -3.

--- work-folder/prep-submit-sandbox.sh
+++ work-folder/prep-submit-sandbox.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Clear submit-sandbox folder
-rm -Rfv ../submit-sandbox/*
+rm -Rfv ../submit-sandbox/
+mkdir ../submit-sandbox/
 
 # copy files
 cp -v ./broadcom-wl-dkms.install ../submit-sandbox
 cp -v ./PKGBUILD ../submit-sandbox
-cp -v ./.AURINFO ../submit-sandbox
+cp -v ./.SRCINFO ../submit-sandbox
 cp -v ./hybrid-v35_64-nodebug-pcoem-6_30_223_248.tar.gz ../submit-sandbox
 cp -v ./broadcom-wl-dkms.conf ../submit-sandbox
 cp -v ./dkms.conf ../submit-sandbox
